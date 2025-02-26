7b71d5f2deb91d2d9ef63ff77aa85d0538ca4e24
Atharva Sardesai <atharvasardesai9@gmail.com>
2025-02-26 13:36:26 +0530

    Time: 3 ms (73.29%), Space: 112 MB (54.77%) - LeetHub

---
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int numOfSubarrays(vector<int>& arr) {
+        const int MOD = 1e9 + 7;
+        int oddCount = 0, evenCount = 1; // Start with evenCount = 1 (for empty subarray)
+        int prefixSum = 0, result = 0;
+
+        for (int num : arr) {
+            prefixSum += num;
+            if (prefixSum % 2 == 0) {
+                result = (result + oddCount) % MOD;
+                evenCount++;
+            } else {
+                result = (result + evenCount) % MOD;
+                oddCount++;
+            }
+        }
+        
+        return result;
+    }
+};
